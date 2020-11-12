b8d8fb0cd8d5ce01f0b018471b89fd8e44d6b69d
Sergey Panov <Sergej.Panov@gmail.com>
2020-11-12 18:41:30 +0100

    Signup backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,18 @@ class UsersController < ApplicationController
     end
 
     def create
-        byebug
+       @user = User.new(user_params)
+       if @user.save
+        flash[:notice] = "You signed up successfully as #{@user.username}!"
+        redirect_to articles_path
+       else
+        render 'new'
+       end
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 end
